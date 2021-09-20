406c6313faa625404839592ff4f03cc3983bb3e9
Evgeny M <moskvitin199@gmail.com>
2021-09-20 22:38:47 +0300

    [Task3] change ErrorBoundary to class component

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
-function ErrorBoundary(props) {
-    const ErrorText = () => {
-      return  (
-        <h2>
-            Oops, something went wrong ... We are doing our best to fix the issue
-        </h2>
-    )}
-
-    let isEverythingOk = true;
-    return (
-        <>
-            {isEverythingOk ? props.children : <ErrorText />}
-        </>
-    )
-}
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+    static getDerivedStateFromError(error) {
+      return { hasError: true };
+    }
+  
+    componentDidCatch(error, errorInfo) {
+      logErrorToMyService(error, errorInfo);
+    }
+  
+    render() {
+      if (this.state.hasError) {
+        return <h1>Oops, something went wrong ... We are doing our best to fix the issue</h1>;
+      }
+  
+      return this.props.children; 
+    }
+  }
 
 export default ErrorBoundary;
--- src/components/movie-list-container/index.jsx
+++ src/components/movie-list-container/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './MovieListContainer.css';
-import mockedMovies from './mockedMovies.json';
+import stubs from './mockedMovies.json';
 import MovieList from '../movie-list';
 import Nav from '../nav';
 import SubTitle from '../sub-title';
@@ -11,7 +11,7 @@ import EmptyComponent from '../empty-component';
 class MovieListContainer extends React.Component {
   constructor() {
     super();
-    this.state = { genre: 'all', sortParam: 'release_date' };
+    this.state = { genre: 'all', sortParam: 'release_date', mockedMovies: stubs };
     this.filterMovies = this.filterMovies.bind(this);
     this.filterByGenre = this.filterByGenre.bind(this);
     this.setSortParam = this.setSortParam.bind(this);
@@ -19,8 +19,8 @@ class MovieListContainer extends React.Component {
   }
 
   filterMovies() {
-    if (this.state.genre === 'all') return mockedMovies;
-    return mockedMovies.filter((movie) => movie.genres.includes(this.state.genre));
+    if (this.state.genre === 'all') return this.state.mockedMovies;
+    return this.state.mockedMovies.filter((movie) => movie.genres.includes(this.state.genre));
   }
 
   filterByGenre(genre) {

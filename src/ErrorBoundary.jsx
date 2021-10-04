ab3571e42ccc13c3058e781de6674a44dd5de1f2
Evgeny M <moskvitin199@gmail.com>
2021-10-04 14:48:15 +0300

    [Тask4] refactor

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 
 export class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    static getDerivedStateFromError(error) {
-      return { hasError: true };
-    }
-  
-    componentDidCatch(error, errorInfo) {
-      console.error(error, errorInfo);
-    }
-  
-    render() {
-      return this.state.hasError 
-        ? <h1>Oops, something went wrong ... We are doing our best to fix the issue</h1> 
-        : this.props.children; 
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
   }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    return this.state.hasError ? (
+      <h1>
+        Oops, something went wrong ... We are doing our best to fix the issue
+      </h1>
+    ) : (
+      this.props.children
+    );
+  }
+}

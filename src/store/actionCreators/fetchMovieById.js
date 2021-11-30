40b039e96338f4db1f495cc0af2a88fe0106409e
Evgeny M <moskvitin199@gmail.com>
2021-11-30 18:56:41 +0300

    [Task10] add return for inner function

--- src/store/actionCreators/fetchMovieById.js
+++ src/store/actionCreators/fetchMovieById.js
@@ -4,7 +4,7 @@ import fetch from "isomorphic-fetch";
 
 export const fetchMovieByIdActionCreator = (movieId) => {
   return function (dispatch) {
-    fetch(`${constants.BASE_URL}/movies/${movieId}`)
+    return fetch(`${constants.BASE_URL}/movies/${movieId}`)
       .then((response) => response.json())
       .then((data) => dispatch(fetchMovieSuccess(data)));
   };
